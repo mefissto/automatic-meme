cec49f7d118e5309a6c4ea5c6ef5f3aeb59e5652
Serhii Maryniuk <mefissto.w@gmail.com>
2024-05-26 10:45:58 +0200

    Created a new Todo List component
    Added a new component called Todo List that includes feature to add and delete tasks. This component includes a HTML file for structure, SCSS file for styling, a TypeScript file for logic, and a spec file for testing. Updated routes to include the new Todo List component.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,5 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TodoListComponent } from './system/todo-list/todo-list.component';
+
+export const routes: Routes = [{ path: 'todos', component: TodoListComponent }];
